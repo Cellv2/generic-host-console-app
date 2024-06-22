e17ef8a275a645fd0256815eb426764ffcf411b3
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-06-22 19:46:03 +0100

    Separate services into own folder

--- CompleteConsoleApp/IFooService.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IFooService
-{
-    void DoThing(int number);
-}
---
+++ CompleteConsoleApp/Services/BarService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class BarService(ILogger<BarService> logger, IFooService fooService) : IBarService
+{
+    public void DoSomeRealWork()
+    {
+        logger.LogInformation("BAWB, DO SUMTIN!");
+
+        for (int i = 0; i < 10; i++)
+        {
+            fooService.DoThing(i);
+        }
+
+        logger.LogInformation("It is done");
+    }
+}
