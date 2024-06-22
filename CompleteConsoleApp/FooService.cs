24dbb806b44d45bbf54d668e63469b014a87600b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-06-22 18:18:05 +0100

    Glory be, it works as generic host

--- CompleteConsoleApp/FooService.cs
+++ CompleteConsoleApp/FooService.cs
@@ -5,19 +5,19 @@ using Microsoft.Extensions.Options;
 public class FooService : IFooService
 {
     private readonly ILogger<FooService> _logger;
-    //private readonly TestSettings _testSettings;
+    private readonly TestSettings _testSettings;
 
-    //public FooService(IOptions<TestSettings> options, ILoggerFactory loggerFactory)
-    public FooService(ILoggerFactory loggerFactory)
+    public FooService(IOptions<TestSettings> options, ILoggerFactory loggerFactory)
+    //public FooService(ILoggerFactory loggerFactory)
     {
         _logger = loggerFactory.CreateLogger<FooService>();
 
-        //_testSettings = options.Value;
+        _testSettings = options.Value;
     }
 
     public void DoThing(int number)
     {
-        //_logger.LogWarning(_testSettings.SecondSetting);
+        _logger.LogWarning(_testSettings.SecondSetting);
 
         _logger.LogInformation($"Doing the thing {number}");
     }
--- CompleteConsoleApp/Program.cs
+++ CompleteConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CompleteConsoleApp.Models.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 
@@ -11,29 +12,52 @@ Console.WriteLine("Hello, World!");
 
 
 
-var services = new ServiceCollection();
-// optional if you want to read the config.
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json")
+//var services = new ServiceCollection();
+//// optional if you want to read the config.
+//var configuration = new ConfigurationBuilder()
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json")
+//    .Build();
+
+//services.AddOptions();
+
+//services.AddOptions<TestSettings>().Configure<IConfiguration>((options, configuration) => configuration.GetSection(nameof(TestSettings)).Bind(options));
+
+//services.AddSingleton(configuration);
+
+//services.AddSingleton<IBarService, BarService>();
+//services.AddSingleton<IFooService, FooService>();
+
+//services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
+
+//using var sp = services.BuildServiceProvider();
+//var requiredService = sp.GetRequiredService<ILogger<Program>>();
+////requiredService.LogError("Test");
+//requiredService.LogInformation("Test");
+
+//var svc = sp.GetService<IBarService>();
+//svc.DoSomeRealWork();
+
+
+
+// we are using the .NET Generic Host builder
+// dotnet add package Microsoft.Extensions.Hosting
+// -----
+// https://learn.microsoft.com/en-us/dotnet/core/extensions/generic-host?tabs=hostbuilder
+// https://stackoverflow.com/questions/68392429/how-to-run-net-core-console-app-using-generic-host-builder
+// https://dfederm.com/building-a-console-app-with-.net-generic-host/
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => {
+
+        services.AddOptions<TestSettings>().Configure<IConfiguration>((options, configuration) => configuration.GetSection(nameof(TestSettings)).Bind(options));
+
+        services.AddSingleton<IBarService, BarService>();
+        services.AddSingleton<IFooService, FooService>();
+
+    })
     .Build();
 
-services.AddOptions();
+var service = host.Services.GetRequiredService<IBarService>();
+service.DoSomeRealWork();
 
-services.AddOptions<TestSettings>().Configure<IConfiguration>((options, configuration) => configuration.GetSection(nameof(TestSettings)).Bind(options));
-
-services.AddSingleton(configuration);
-
-services.AddSingleton<IBarService, BarService>();
-services.AddSingleton<IFooService, FooService>();
-
-services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
-
-using var sp = services.BuildServiceProvider();
-var requiredService = sp.GetRequiredService<ILogger<Program>>();
-//requiredService.LogError("Test");
-requiredService.LogInformation("Test");
-
-var svc = sp.GetService<IBarService>();
-svc.DoSomeRealWork();
-
+// shouldn't need to use host.Run() here because we actually want it to spin down once completed
